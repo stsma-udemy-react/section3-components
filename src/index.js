ec0ba4f8b1128fa0a22c3041cc4ddc9dd5fc6f9a
stephensmakilan <stephensmakilan@gmail.com>
2021-02-15 23:13:39 +0800

    adding faker image api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import faker, { fake } from 'faker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = ()=> {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image() }/>
+                </a>
+                <div className="content"> 
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                </div>
+                <div className="metadata">
+                    <span className="date">Today at 6:00PM</span>
+                </div>
+                <div className="text">
+                    Nice blog post!
+                </div>
+            </div>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
