e90fe4af3e85c82dfdd51f26657f1cbe6136e6bb
stephensmakilan <stephensmakilan@gmail.com>
2021-02-16 17:14:04 +0800

    End of Section 3

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import faker, { fake } from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-const App = ()=> {
+const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.image() }/>
-                </a>
-                <div className="content"> 
-                    <a href="/" className="author">
-                        Sam
-                    </a>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
                 </div>
-                <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
-                </div>
-                <div className="text">
-                    Nice blog post!
-                </div>
-            </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Sam' 
+                    timeAgo="Today at 6:00PM" 
+                    blogPost="Nice blog post!" 
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Alex' 
+                    timeAgo="Today at 4:45PM" 
+                    blogPost="Great notes." 
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Jane' 
+                    timeAgo="Today at 8:50PM" 
+                    blogPost="Hooray! Perfect." 
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
         </div>
     )
 }
